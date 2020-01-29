34b3019cdffd7d3f78c8d2e1766916aab6c9e192
Alan Rabelo Martins <alanrabelo@icloud.com>
2020-01-29 14:01:12 -0300

    Initial Commit

---
+++ Quiz Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz Challenge
+//
+//  Created by Alan Martins on 29/01/20.
+//  Copyright © 2020 Alan Rabelo Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
